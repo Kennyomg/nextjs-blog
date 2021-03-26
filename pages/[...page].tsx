9559da47bc47bfa059f8d83b5a8b33163f8eb86c
Kenrick Halff <kenrickhalff@gmail.com>
2021-03-26 12:10:04 +0100

    Added builderpage catch all and api key

---
+++ pages/[...page].tsx
@@ -0,0 +1,48 @@
+import { builder, BuilderComponent } from '@builder.io/react'
+import { GetStaticProps, GetStaticPaths, GetServerSideProps } from 'next'
+
+builder.init(process.env.REACT_APP_BUILDER_API_KEY)
+
+export default function BuilderPage(props) {
+    return (<>
+    {props.content && 
+        <BuilderComponent
+            content={props.content}
+            model="page" />
+    }
+    </>)
+}
+
+// Get page data
+// https://nextjs.org/docs/basic-features/data-fetching#getstaticprops-static-generation
+export const getStaticProps: GetStaticProps = async (context) => {
+    const content = await builder.get('page', { url: context.params.url }).promise();
+
+    return {
+        props: { content },
+        revalidate: true,
+        notFound: !content
+    }
+}
+
+// List all Builder pages
+// https://nextjs.org/docs/basic-features/data-fetching#getstaticpaths-static-generation
+export const getStaticPaths: GetStaticPaths = async () => {
+    const results = await builder.getAll('page', {
+        fields: 'data.url',
+        key: 'all-pages',
+        limit: 200,
+        options: {
+            noTargeting: true
+        }
+    })
+
+    return {
+        paths: results.map((item) => ({
+            params: {
+                page: item.data.url.substr(1)
+            }
+        })),
+        fallback: true
+    }
+}
