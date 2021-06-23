739e218bd2cb5ab044c8c507af259c27817a0957
Kenrick Halff <kenrickhalff@gmail.com>
2021-06-23 11:51:13 +0200

    Added icons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,35 +17,41 @@ class MyDocument extends Document {
             <meta name='description' content='Your own Happy Jar' />
             <meta name='format-detection' content='telephone=no' />
             <meta name='mobile-web-app-capable' content='yes' />
-            <meta name='msapplication-config' content='/public/icons/browserconfig.xml' />
+            <meta name='msapplication-config' content='/icons/browserconfig.xml' />
             <meta name='msapplication-TileColor' content='#2B5797' />
             <meta name='msapplication-tap-highlight' content='no' />
             <meta name='theme-color' content='#000000' />
 
-            <link rel='apple-touch-icon' href='/static/icons/touch-icon-iphone.png' />
-            <link rel='apple-touch-icon' sizes='152x152' href='/static/icons/touch-icon-ipad.png' />
-            <link rel='apple-touch-icon' sizes='180x180' href='/static/icons/touch-icon-iphone-retina.png' />
-            <link rel='apple-touch-icon' sizes='167x167' href='/static/icons/touch-icon-ipad-retina.png' />
+            <link rel='apple-touch-icon' href='/icons/Jar-iphone.png' />
+            <link rel="apple-touch-icon" sizes="57x57" href="/icons/Jar-iphone.png">
+            <link rel="apple-touch-icon" sizes="76x76" href="/icons/Jar-ipad-small.png">
+            <link rel="apple-touch-icon" sizes="120x120" href="/icons/Jar-iphone-retina.png">
+            <link rel='apple-touch-icon' sizes='152x152' href='/icons/Jar-ipad.png' />
+            <link rel='apple-touch-icon' sizes='167x167' href='/icons/Jar-ipad-retina.png' />
+            <link rel='apple-touch-icon' sizes='180x180' href='/icons/Jar-iphone-retina.png' />
 
-            <link rel='icon' type='image/png' sizes='32x32' href='/static/icons/favicon-32x32.png' />
-            <link rel='icon' type='image/png' sizes='16x16' href='/static/icons/favicon-16x16.png' />
+            <link rel="icon" sizes="192x192" href="/icons/Jar-192x192.png" />
+            <link rel="icon" sizes="128x128" href="/icons/Jar-128x128.png" />
+
+            <link rel='icon' type='image/png' sizes='32x32' href='/icons/Jar-32x32.png' />
+            <link rel='icon' type='image/png' sizes='16x16' href='/icons/Jar-16x16.png' />
             <link rel='manifest' href='/manifest.json' />
-            <link rel='mask-icon' href='/static/icons/safari-pinned-tab.svg' color='#5bbad5' />
-            <link rel='shortcut icon' href='/favicon.ico' />
+            <link rel='mask-icon' href='/icons/safari-pinned-tab.svg' color='#5bbad5' />
+            <link rel='shortcut icon' type='image/png' href='/icons/Jar-16x16.png' />
             <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:300,400,500' />
                 
             <meta name='twitter:card' content='summary' />
-            <meta name='twitter:url' content='https://yourdomain.com' />
+            <meta name='twitter:url' content='https://nextjs-blog-kennyomg.vercel.app' />
             <meta name='twitter:title' content='Happy Jar' />
             <meta name='twitter:description' content='Your own Happy Jar' />
-            <meta name='twitter:image' content='https://yourdomain.com/static/icons/android-chrome-192x192.png' />
-            <meta name='twitter:creator' content='@DavidWShadow' />
+            <meta name='twitter:image' content='/icons/Jar-192x192.png' />
+            <meta name='twitter:creator' content='@KennyOmg' />
             <meta property='og:type' content='website' />
             <meta property='og:title' content='Happy Jar' />
             <meta property='og:description' content='Your own Happy Jar' />
             <meta property='og:site_name' content='Happy Jar' />
-            <meta property='og:url' content='https://yourdomain.com' />
-            <meta property='og:image' content='https://yourdomain.com/static/icons/apple-touch-icon.png' />
+            <meta property='og:url' content='https://nextjs-blog-kennyomg.vercel.app' />
+            <meta property='og:image' content='https://nextjs-blog-kennyomg.vercel.app/icons/apple-touch-icon.png' />
 
             {/* {'<!--'} apple splash screen images {'-->'}
             {'<!--'}
