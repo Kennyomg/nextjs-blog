3887f6aa10b1157723a644517fbcc7547e3df299
Kenrick Halff <kenrickhalff@gmail.com>
2021-09-07 18:11:05 +0200

    Try again with no fauna

--- pages/api/getCustomers/index.ts
+++ pages/api/getCustomers/index.ts
@@ -1,25 +1,25 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-import faunadb from 'faunadb'
+// import { NextApiRequest, NextApiResponse } from 'next'
+// import faunadb from 'faunadb'
 
-const secret = process.env.FAUNADB_SECRET_KEY
-const q = faunadb.query;
-const client = new faunadb.Client({ secret })
+// const secret = process.env.FAUNADB_SECRET_KEY
+// const q = faunadb.query;
+// const client = new faunadb.Client({ secret })
 
-export default async (_: NextApiRequest, res: NextApiResponse) => {
-    try {
-        const dbs = await client.query(
-            q.Map(
-                q.Paginate(
-                    q.Match(
-                        q.Index('all_customers')
-                    )
-                ),
-                ref => q.Get(ref)
-            )
-        ) as { data: object }
-        res.status(200).json(dbs.data)
-    }
-    catch(error) {
-        res.status(500).json({Error: error.message})
-    }
-}
+// export default async (_: NextApiRequest, res: NextApiResponse) => {
+//     try {
+//         const dbs = await client.query(
+//             q.Map(
+//                 q.Paginate(
+//                     q.Match(
+//                         q.Index('all_customers')
+//                     )
+//                 ),
+//                 ref => q.Get(ref)
+//             )
+//         ) as { data: object }
+//         res.status(200).json(dbs.data)
+//     }
+//     catch(error) {
+//         res.status(500).json({Error: error.message})
+//     }
+// }
